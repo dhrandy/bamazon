09567dc0f9263f7f78e8d43ad83926ad64654c51
dhrandy <dhrandy@gmail.com>
2018-06-03 11:26:54 -0400

    Updated all code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,114 +1,124 @@
 require("dotenv").config()
 var inquirer = require("inquirer")
-
-var mysql = require("mysql");
+var mysql = require("mysql")
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.password,
-  database: "spitfireDB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.password,
+    database: "spitfireDB"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-   queryAllProducts()
-   purchase()
-  //  connection.end()
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start()
+    queryAllProducts()
+    purchase()
+        //  connection.end()
 });
 
-function queryAllProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    console.log("######  ")                                          
+function start() {
+    console.log("######  ")
     console.log("#     #   ##   #    #   ##   ######  ####  #    #")
-    console.log("#     #  #  #  ##  ##  #  #      #  #    # ##   #") 
-    console.log("######  #    # # ## # #    #    #   #    # # #  #") 
-    console.log("#     # ###### #    # ######   #    #    # #  # #") 
-    console.log("#     # #    # #    # #    #  #     #    # #   ##") 
+    console.log("#     #  #  #  ##  ##  #  #      #  #    # ##   #")
+    console.log("######  #    # # ## # #    #    #   #    # # #  #")
+    console.log("#     # ###### #    # ######   #    #    # #  # #")
+    console.log("#     # #    # #    # #    #  #     #    # #   ##")
     console.log("######  #    # #    # #    # ######  ####  #    #")
     console.log("")
-    console.log("--------------------------------------------------6")
-    console.log("")
-    console.log("ID | " + "Item | " + "Department | " + "Price | " + "Quantity")
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity)
-    }
-  });
+    console.log("Welcome to Bamazom! Enjoy your shopping!")
+}
+
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.log("")
+        console.log("--------------------------------------------------")
+        console.log("")
+        console.log("ID | " + "Item | " + "Department | " + "Price | " + "Quantity")
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + "$" + res[i].price + " | " + res[i].stock_quantity)
+        }
+        console.log("")
+        console.log("--------------------------------------------------")
+        console.log("")
+    });
 }
 
 function purchase() {
-  // query the database for all items being products
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to purchase
-    // console.log(results)
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "rawlist",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < res.length; i++) {
-              choiceArray.push(res[i].product_name);
-            }
-            return choiceArray;
-          },
-          message: "What is the ID of the product you'd like to purchase?"
-        },
-        {
-          name: "quantity",
-          type: "input",
-          message: "How many would you like to buy?"
-        }
-        
-      ])
-      // console.log(stock_quantity)
-      .then(function(answer) {
-        // get the information of the chosen item
-        var chosenItem;
-        for (var i = 0; i < res.length; i++) {
-          if (res[i].product_name === answer.choice) {
-            chosenItem = res[i]
-            // console.log(chosenItem)
-            // console.log(chosenItem.stock_quantity)
-            // console.log(answer.quantity)
-          }
-        }
+    // query the database for all items being products
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        // once you have the items, prompt the user for which they'd like to purchase
+        // console.log(results)
+        inquirer
+            .prompt([{
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function() {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].product_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What is the ID of the product you'd like to purchase?"
+                }, {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to buy?"
+                }
 
-        // determine if stock quantity is higher than entered quantity
-        // console.log(chosenItem.stock_quantity)
-        //     console.log(answer.quantity)
-        if (parseInt(answer.quantity) < parseInt(chosenItem.stock_quantity)) {
-          var newQuantity = (parseInt(chosenItem.stock_quantity) - parseInt(answer.quantity))
-          console.log(newQuantity)
-          console.log(chosenItem.item_id)
-          console.log(chosenItem.product_name)
-          // bid was high enough, so update db, let the user know, and start over
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock_quantity: newQuantity
-              },
-              {
-                item_id: parseInt(chosenItem.item_id)
-              }
-            ],
-            function(err) {
-              if (err) throw err
-              console.log("Purchase placed successfully!")
-              connection.end()
-            }
-          )
-        }
-        else {
-          // bid wasn't high enough, so apologize and start over
-          console.log("Inventory too low...");
-          
-        }
-      });
-  });
+            ])
+            .then(function(answer) {
+                // get the information of the chosen item
+                var chosenItem;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].product_name === answer.choice) {
+                        chosenItem = res[i]
+                    }
+                }
+
+                // determine if stock quantity is higher than entered quantity
+                if (parseInt(answer.quantity) < parseInt(chosenItem.stock_quantity)) {
+                    var newQuantity = (parseInt(chosenItem.stock_quantity) - parseInt(answer.quantity))
+
+                    // bid was high enough, so update db, let the user know, and start over
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?", [{
+                            stock_quantity: newQuantity
+                        }, {
+                            item_id: parseInt(chosenItem.item_id)
+                        }],
+                        function(err) {
+                            if (err) throw err
+                            console.log("")
+                            console.log("--------------------------------------------------")
+                            console.log("")
+                            console.log("Purchase placed successfully!")
+                            console.log("Total Cost: $" + (parseInt(answer.quantity) * parseInt(chosenItem.price)))
+                            queryAllProducts()
+                            purchase()
+                            wait(2000)
+                                // connection.end()
+                        }
+                    )
+                } else {
+                    // bid wasn't high enough, so apologize and start over
+                    console.log("Inventory too low...");
+                    wait(2000);
+                    queryAllProducts()
+                    purchase()
+                }
+            });
+    });
+}
+
+function wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+        end = new Date().getTime();
+    }
 }
---
+++ bamazonManager.js
@@ -0,0 +1,201 @@
+////////////////////////////////////////////////////
+// required files
+////////////////////////////////////////////////////
+require("dotenv").config()
+var inquirer = require("inquirer")
+var mysql = require("mysql")
+
+////////////////////////////////////////////////////
+// Creates a connection to the mysql database
+////////////////////////////////////////////////////
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.password,
+    database: "spitfireDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start()
+        //  connection.end()
+});
+////////////////////////////////////////////////////
+// function which prompts the user for what action they should take aka the start of the program
+////////////////////////////////////////////////////
+function start() {
+    inquirer
+        .prompt({
+            name: "manager",
+            type: "rawlist",
+            message: "MAKE A SELECTION",
+            choices: ["VIEW PRODUCTS FOR SALE", "VIEW LOW INVENTORY", "ADD INVENTORY", "ADD NEW PRODUCT"]
+        })
+
+    .then(function(answer) {
+        // based on their answer, either call the correct function
+        if (answer.manager === "VIEW PRODUCTS FOR SALE") {
+            queryAllProducts();
+        } else if (answer.manager === "VIEW LOW INVENTORY") {
+            viewLow();
+        } else if (answer.manager === "ADD INVENTORY") {
+            addInvent();
+        } else if (answer.manager === "ADD NEW PRODUCT") {
+            addNewProduct();
+        } else {
+            console.log("INVALID SELECTION, TRY AGAIN.")
+            start();
+        }
+    });
+}
+////////////////////////////////////////////////////
+// function which displays all the products
+////////////////////////////////////////////////////
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.log("")
+        console.log("--------------------------------------------------")
+        console.log("")
+        console.log("ID | " + "Item | " + "Department | " + "Price | " + "Quantity")
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + "$" + res[i].price + " | " + res[i].stock_quantity)
+        }
+        console.log("")
+        console.log("--------------------------------------------------")
+        console.log("")
+        start()
+    });
+}
+////////////////////////////////////////////////////
+// function which displays only the low inventory
+////////////////////////////////////////////////////
+function viewLow() {
+
+}
+////////////////////////////////////////////////////
+// function which displays only the inventory, this is used in the add inventory function
+////////////////////////////////////////////////////
+function invent() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log("")
+        console.log("--------------------------------------------------")
+        console.log("")
+        console.log("ID | " + "Item | " + "Quantity")
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].stock_quantity)
+        }
+        console.log("")
+        console.log("--------------------------------------------------")
+        console.log("")
+    })
+}
+////////////////////////////////////////////////////
+// function which allows you to add to the inventory
+////////////////////////////////////////////////////
+function addInvent() {
+    invent()
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([{
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function() {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].product_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What is the ID of the product you'd like to add inventory to?"
+                }, {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to add?"
+                }
+            ])
+
+        .then(function(answer) {
+            // get the information of the chosen item
+            var chosenItem;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].product_name === answer.choice) {
+                    chosenItem = res[i]
+                }
+            }
+
+            connection.query(
+                "UPDATE products SET ? WHERE ?", [{
+                    stock_quantity: (parseInt(answer.quantity) + parseInt(chosenItem.stock_quantity))
+                }, {
+                    product_name: chosenItem.product_name
+                }],
+                function(err) {
+                    if (err) throw err
+                    console.log("")
+                    console.log("--------------------------------------------------")
+                    console.log("")
+                    console.log("Inventory added successfully!")
+                    console.log("--------------------------------------------------")
+                    console.log("")
+                    start()
+                }
+            )
+        })
+    })
+}
+////////////////////////////////////////////////////
+// function which allows you to add new product to the database
+////////////////////////////////////////////////////
+function addNewProduct() {
+    inquirer
+        .prompt([{
+                name: "productName",
+                type: "input",
+                message: "What product would you like to add?"
+            },
+
+            {
+                name: "productDepartment",
+                type: "input",
+                message: "What department would you like to add the product to?"
+            },
+
+            {
+                name: "productPrice",
+                type: "input",
+                message: "What is the price of the new product?"
+            },
+
+            {
+                name: "productQuantity",
+                type: "input",
+                message: "What is the quantity of the new product you're adding to the inventory?"
+            }
+
+        ])
+
+    .then(function(answer) {
+        // when finished prompting, insert a new item into the db with that info
+        connection.query(
+            "INSERT INTO products SET ?", {
+                product_name: answer.productName,
+                department_name: answer.productDepartment,
+                price: answer.productPrice,
+                stock_quantity: answer.productQuantity
+            },
+            function(err) {
+                if (err) throw err;
+                console.log("Product added successfully!");
+                console.log("--------------------------------------------------")
+                console.log("")
+                    // re-prompt the user for if they want to bid or post
+                start();
+            }
+        );
+    });
+}
